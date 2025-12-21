cfaea0f6bacf1bd907afb5a561d0ec8cbdfd78d7
balenziphillip <balensphillip7@gmail.com>
2025-12-21 07:07:31 +0300

    Fixed some bugs on the class variables on the Login page

--- iams/src/features/auth/Login.jsx
+++ iams/src/features/auth/Login.jsx
@@ -4,21 +4,14 @@ import useTheme from "../../hooks/useTheme";
 
 export default function Login() {
    const { theme, setTheme } = useTheme();
-  const [showPassword, setShowPassword] = useState(false);
+   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="min-h-screen flex items-center justify-center px-4 bg-[rgb(var(--background))] text-[rgb(var(--foreground))]">
          {/* 🌗 Theme Toggle */}
-        <button
-          onClick={() =>
-            setTheme(theme === "dark" ? "light" : "dark")
-          }
+        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           className="absolute top-4 right-4 p-2 rounded-lg bg-[rgb(var(--card))] border border-[rgb(var(--border))] hover:opacity-80 transition" >
-          {theme === "dark" ? (
-            <Sun className="h-4 w-4 text-[rgb(var(--text))]" />
-          ) : (
-            <Moon className="h-4 w-4 text-[rgb(var(--text))]" />
-          )}
+          {theme === "dark" ? (<Sun className="h-4 w-4 text-[rgb(var(--text))]"/>) : (<Moon className="h-4 w-4 text-[rgb(var(--text))]"/>)}
         </button>
 
         {/* Login Card */}
@@ -27,51 +20,37 @@ export default function Login() {
         {/* Logo */}
         <div className="flex justify-center mb-6">
             <span className="text-white font-semibold text-xl">
-              <img src="../../public/nwsc-logo.jfif" alt="Logo" className="h-20 w-20 rounded-xl" />
+              <img src="/nwsc-logo.jfif" alt="Logo" className="h-20 w-20 rounded-xl" />
             </span>
         </div>
 
         {/* Title */}
-        <h1 className="text-2xl font-semibold text-center text-[rgb(var(--text))]">
-          Internal Audit Management System
-        </h1>
-        <p className="text-sm text-center mt-1 text-[rgb(var(--text))]">
-          Enter your HRMS credentials to log in.
-        </p>
+        <h1 className="text-2xl font-semibold text-center text-[rgb(var(--text))]">Internal Audit Management System</h1>
+        <p className="text-sm text-center mt-1 text-[rgb(var(--text))]">Enter your HRMS credentials to log in</p>
 
         {/* Form */}
         <form className="mt-8 space-y-5">
           
           {/* Username */}
           <div>
-            <label className="block text-sm mb-1 text-[rgb(var(--text))]">
-              Username
-            </label>
+            <label className="block text-sm mb-1 text-[rgb(var(--text))]">Username</label>
             <div className="relative">
               <User className="absolute left-3 top-3.5 h-4 w-4 text-slate-400" />
-              <input
-                type="text"
-                placeholder="Enter username"
-                className="w-full pl-10 pr-4 py-3 rounded-lg bg-slate-800 text-white placeholder-text-[rgb(var(--text))] border border-slate-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
+              <input type="text" placeholder="Enter username"
+                className="w-full pl-10 pr-4 py-3 rounded-lg bg-[rgb(var(--background))] text-[rgb(var(--text))] placeholder-text-[rgb(var(--text))] border border-[rgb(var(--border))] focus:outline-none focus:ring-2 focus:ring-blue-600"
               />
             </div>
           </div>
 
           {/* Password */}
           <div>
-            <label className="block text-sm mb-1 text-[rgb(var(--text))]">
-              Password
-            </label>
+            <label className="block text-sm mb-1 text-[rgb(var(--text))]">Password</label>
             <div className="relative">
               <Lock className="absolute left-3 top-3.5 h-4 w-4 text-slate-400" />
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Enter password"
-                className="w-full pl-10 pr-10 py-3 rounded-lg bg-slate-800 text-white placeholder-text-[rgb(var(--text))] border border-slate-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
+              <input type={showPassword ? "text" : "password"} placeholder="Enter password"
+                className="w-full pl-10 pr-10 py-3 rounded-lg bg-[rgb(var(--background))] text-[rgb(var(--text))] placeholder-text-[rgb(var(--text))] border border-[rgb(var(--border))] focus:outline-none focus:ring-2 focus:ring-blue-600"
               />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
+              <button type="button" onClick={() => setShowPassword(!showPassword)}
                 className="absolute right-3 top-3.5 text-slate-400 hover:text-slate-200"
               >
                 {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
@@ -81,27 +60,18 @@ export default function Login() {
 
           {/* Forgot password */}
           <div className="flex justify-end">
-            <a
-              href="#"
-              className="text-sm text-blue-500 hover:underline"
-            >
-              Forgot Password?
-            </a>
+            <a href="#" className="text-sm text-blue-500 hover:underline">Forgot Password?</a>
           </div>
 
           {/* Submit */}
-          <button
-            type="submit"
+          <button type="submit"
             className="w-full py-3 rounded-lg bg-blue-600 hover:bg-blue-700 text-white font-semibold transition flex items-center justify-center gap-2"
-          >
-            Sign In →
+          > Sign In →
           </button>
         </form>
 
         {/* Footer */}
-        <p className="text-xs text-[rgb(var(--text))] text-center mt-6">
-          © {new Date().getFullYear()} NWSC – Applications Department
-        </p>
+        <p className="text-xs text-[rgb(var(--text))] text-center mt-6"> © {new Date().getFullYear()} NWSC – Applications Department</p>
       </div>
     </div>
   );
